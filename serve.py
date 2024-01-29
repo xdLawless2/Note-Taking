c90618516a6f9775c851a591b3509909bf4956e6
Aditya Singh <getaditya2008@gmail.com>
2024-01-29 17:11:13 +0100

    test

--- serve.py
+++ serve.py
@@ -46,6 +46,8 @@ def summarize():
 def open_browser():
       webbrowser.open_new('http://127.0.0.1:5000/')
 
+print(1)
+
 if __name__ == '__main__':
     Timer(1, open_browser).start()
     app.run(port=5000)
